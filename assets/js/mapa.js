a51e5dcdde83a54e8490c4d49ab3d2318c688a77
dagon8523 <dagon8523@gmail.com>
2020-09-09 17:01:26 -0500

    se realiza insercion de mapa

---
+++ assets/js/mapa.js
@@ -0,0 +1,10 @@
+const tilesProvider='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+
+let mymap=L.map('map').setView([51.505,-0.09],13)
+
+L.tileLayer(tilesProvider,{
+    maxZoom:18,
+}).addTo(mymap)
+
+
+ 
